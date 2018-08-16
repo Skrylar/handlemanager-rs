e759c3959d13a59a3d59ef6fc5a07d9f59738b35
Joshua Cearley <joshua.cearley@gmail.com>
2018-08-15 20:26:31 -0500

    Handle management where no free-list is used.

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,26 @@ pub enum AllocPolicy {
     RecycleLowest,
 }
 
+#[derive(Debug)]
+pub enum ReleasePolicy {
+    /// Free handles are never tracked, and thus never re-used.
+    DontTrack,
+}
+
 pub struct HandleManager {
-    alloc_policy: AllocPolicy
+    alloc_policy: AllocPolicy,
+    release_policy: ReleasePolicy,
+    highest: usize,
+    highest_ever: Option<usize>,
 }
 
 impl Default for HandleManager {
     fn default() -> Self {
         HandleManager{
             alloc_policy: AllocPolicy::RecycleLowest,
+            release_policy: ReleasePolicy::DontTrack,
+            highest: 0,
+            highest_ever: None,
         }
     }
 }
@@ -23,16 +35,38 @@ impl HandleManager {
     }
 
     pub fn with_alloc_policy(mut self, policy: AllocPolicy) -> Self {
+        if self.highest_ever.is_some() {
+            panic!("Cannot change allocation policy once handles have been dispensed.")
+        }
+
         self.alloc_policy = policy;
         self
     }
 
+    pub fn with_release_policy(mut self, policy: ReleasePolicy) -> Self {
+        if self.highest_ever.is_some() {
+            panic!("Cannot change release policy once handles have been dispensed.")
+        }
+
+        self.release_policy = policy;
+        self
+    }
+
     pub fn next(&mut self, ) -> usize {
-        unimplemented!()
+        match &self.release_policy {
+            ReleasePolicy::DontTrack => {
+                let result = self.highest;
+                self.highest += 1;
+                self.highest_ever = Some(result);
+                result
+            }
+        }
     }
 
     pub fn release(&mut self, _handle: usize) {
-        unimplemented!()
+        match &self.release_policy {
+            ReleasePolicy::DontTrack => {/* nothing to do */},
+        }
     }
 }
 
--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,22 @@
 use ::HandleManager;
 
 #[test]
-fn basic_use() {
+fn simplest_use() {
+    let mut x = HandleManager::new()
+        .with_release_policy(::ReleasePolicy::DontTrack)
+        .with_alloc_policy(::AllocPolicy::RecycleLowest);
+
+    assert_eq!(x.next(), 0);
+    assert_eq!(x.next(), 1);
+    assert_eq!(x.next(), 2);
+
+    x.release(1);
+
+    assert_eq!(x.next(), 3);
+}
+
+#[test]
+fn basic_recycling() {
     let mut x = HandleManager::new().with_alloc_policy(::AllocPolicy::RecycleLowest);
 
     assert_eq!(x.next(), 0);
